e845ef3586e2c4853695dfb6686dda8421366eda
shawn <shawnantonucci2018@gmail.com>
2018-12-08 21:04:43 -0800

    setup mongoose.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const keys = require('./config/keys.js');
 require('./services/passport.js');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -7,6 +11,3 @@ require('./routes/authRoutes.js')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-// Google API
-// https://console.developers.google.com
