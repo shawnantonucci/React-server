f1587a1aef5d2cc5e6e4d34d8b605e2e51b2766e
shawn <shawnantonucci2018@gmail.com>
2018-12-08 21:33:21 -0800

    setup database schema

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const keys = require('./config/keys.js');
-require('./services/passport.js');
 require('./models/User.js');
+require('./services/passport.js');
 
 mongoose.connect(keys.mongoURI);
 
