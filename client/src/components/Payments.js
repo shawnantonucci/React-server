0db2aa2d6749eac7a396322e293a41ee67126f85
shawn <shawnantonucci2018@gmail.com>
2018-12-11 14:54:36 -0800

    created handle payment token

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends Component {
     render() {
@@ -10,9 +12,11 @@ class Payments extends Component {
                 amount={500}
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn">Credits</button>
+            </StripeCheckout>
         );
     }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
