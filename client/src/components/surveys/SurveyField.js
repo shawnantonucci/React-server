2f514d298d1739c279b2d549c07d5512c1acef48
shawn <shawnantonucci2018@gmail.com>
2018-12-17 20:56:40 -0800

    created a validateEmail function and validation for the form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,11 +2,14 @@
 // label and text input
 import React from 'react';
 
-export default ({ input, label }) => {
+export default ({ input, label, meta: { error, touched } }) => {
   return (
-      <div> 
+      <div style={{ marginTop: '20px' }}> 
           <label>{label}</label>
-          <input {...input}/>
+          <input {...input} style={{ marginBottom: '5px' }}/>
+          <div className="red-text" style={{ marginBottom: '20px' }}>
+            {touched && error}
+          </div>
       </div>
   )
 }
